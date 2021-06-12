40525c57bdd31197d5a3609abbc069b99c4811b6
Luishsk93 <hyungsunkangkr@gmail.com>
2021-06-12 19:18:50 -0300

    O tamanho do quadro de pixels passa a ser definido pelo usuario. Delimita o tamanho minimo e maximo do board. As cores da paleta dsao geradas aleatoriamente ao carregar a pagina

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const listaCores = document.getElementsByClassName('color');
-
-const possiveisCores = ['black', 'blue', 'yellow', 'red'];
-
+const possiveisCores = ['black', geraCorAleatoria(), geraCorAleatoria(), geraCorAleatoria()];
 const largura = 5;
-
 const comprimento = 5;
-
 const linhas = document.querySelectorAll('.linha');
+const quadroInicial = document.getElementById('pixel-board');
+const botaoVQV = document.getElementById('generate-board');
+const input = document.getElementById('board-size');
+let valorInput = 0;
+const botao = document.getElementById('clear-board');
+
+// Preenche o quadro
+for (let i = 0; i < comprimento; i += 1) {
+  for (let j = 0; j < largura; j += 1) {
+    linhas[i].appendChild(criaQuadro());
+    coresPaleta();
+    selecionado();
+    mudaSelecionado();
+    // Usei como referencia https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onclick_dom. Metodo alternativo ao botao.addEventListener('click', limpaQuadro).
+    botao.onclick = limpaQuadro;
+  }
+}
+// Pinta o quadro inicial 5x5
+const pixels = document.querySelectorAll('.pixel');
+pixelsAPintar();
+
+// Pinta o quadro inicial 5x5
+botaoVQV.addEventListener('click', inputar);
+botaoVQV.addEventListener('click', apagaLinhas);
+botaoVQV.addEventListener('click', fazNovasLinhas);
+botaoVQV.addEventListener('click', adicionaQuadrosePinta);
+
 
 // Adiciona cores na paleta
 function coresPaleta() {
@@ -15,8 +38,6 @@ function coresPaleta() {
     cor.style.backgroundColor = possiveisCores[i];
   }
 }
-coresPaleta();
-
 // Cria quadro individual
 function criaQuadro() {
   const quadro = document.createElement('div');
@@ -24,16 +45,6 @@ function criaQuadro() {
   quadro.style.backgroundColor = 'white';
   return quadro;
 }
-
-// Preenche o quadro
-function preencheQuadro(comp, larg) {
-  for (let i = 0; i < comp; i += 1) {
-    for (let j = 0; j < larg; j += 1) {
-      linhas[i].appendChild(criaQuadro());
-    }
-  }
-}
-preencheQuadro(comprimento, largura);
 
 function selecionado() {
   let blackIndex = -Infinity;
@@ -45,8 +56,6 @@ function selecionado() {
   const cor = listaCores[blackIndex];
   cor.className = 'color selected';
 }
-
-selecionado();
 
 function mudaClasse(event) {
   const x = event.target;
@@ -62,10 +71,6 @@ function mudaSelecionado() {
   }
 }
 
-mudaSelecionado();
-
-const pixels = document.querySelectorAll('.pixel');
-
 function pinta(event) {
   const corSelecionada = document.querySelectorAll('.color.selected')[0].style.backgroundColor;
   for (let i = 0; i < pixels.length; i += 1) {
@@ -80,15 +85,69 @@ function pixelsAPintar() {
   }
 }
 
-pixelsAPintar();
-
-var botao = document.getElementById('clear-board');
-
 function limpaQuadro() {
+  const pixels = document.querySelectorAll('.pixel');
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
   }
 }
 
-// Usei como referencia https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onclick_dom. Metodo alternativo ao botao.addEventListener('click', limpaQuadro).
-botao.onclick = limpaQuadro;
+// Recebe o valor do comprimento do quadro a ser criado
+function inputar() {
+  if (input.value === '') {
+    alert('Board inválido!');
+  } else if (parseFloat(input.value) < 5) {
+    valorInput = 5;
+  } else if (parseFloat(input.value) > 50) {
+    valorInput = 50;
+  } else {
+    valorInput = parseFloat(input.value);
+  }
+}
+// Apaga as linhas existentes
+function apagaLinhas() {
+  const linhas = document.querySelectorAll('.linha');
+  if (valorInput > 5 && valorInput <= 50) {
+    for (let i = 0; i < linhas.length; i += 1) {
+      linhas[i].remove();
+    }
+  }
+}
+// Cria novas linhas, com a quantidade igual ao valor inputado
+function fazNovasLinhas() {
+  if (valorInput > 5 && valorInput <= 50) {
+    for (let i = 0; i < valorInput; i += 1) {
+      const linha = document.createElement('div');
+      linha.className = 'linha';
+      quadroInicial.appendChild(linha);
+    }
+  }
+}
+// Adiciona os pixels nas linhas criadas, e pinta. Obs: Sei que não é o ideal ter uma função que faz mais de uma coisa, mas não encontrei uma outra forma de criar novas linhas e pintá-las depois em funcoes diferentes.
+function adicionaQuadrosePinta() {
+  const linhas = document.querySelectorAll('.linha');
+  if (valorInput > 5 && valorInput <= 50) {
+    for (let i = 0; i < valorInput; i += 1) {
+      for (let j = 0; j < valorInput; j += 1) {
+        linhas[i].appendChild(criaQuadro());
+      }
+    }
+  }
+  const pixels = document.querySelectorAll('.pixel');
+  function pinta(event) {
+    const corSelecionada = document.querySelectorAll('.color.selected')[0].style.backgroundColor;
+    for (let i = 0; i < pixels.length; i += 1) {
+      const x = event.target;
+      x.style.backgroundColor = corSelecionada;
+    }
+  }
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', pinta);
+  }
+}
+
+// Usei como referencia esse site https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
+function geraCorAleatoria() {
+  var corAleatoria = '#'+Math.floor(Math.random()*16777215).toString(16);
+  return corAleatoria;
+}
